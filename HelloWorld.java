1372a6b6da04b086f88a0a1f9dd0e3e33cc6b8b5
Natalia Sharshakova <sharshanatty@gmail.com>
2021-09-16 11:17:40 +0300

    Merge two branches and get conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 public class Hello {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Hello, world!1");
+=======
+		System.out.println("Hello, world!\n");
+>>>>>>> 1b091a9 (Changed string end)
 		System.out.println("New Feature!");
 	}
 

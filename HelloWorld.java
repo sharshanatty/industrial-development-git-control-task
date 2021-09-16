1fa980524865974dc59852cfef07567810bc0d80
Natalia Sharshakova <sharshanatty@gmail.com>
2021-09-16 11:40:00 +0300

    Chenged second string end

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!\n");
-		System.out.println("New Feature!");
+		System.out.println("New Feature!\n");
 	}
 
 }
